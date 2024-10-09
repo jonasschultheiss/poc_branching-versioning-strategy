9f9ee410ef2f3b2d2710efc1b4242d53f164b1f9
Jonas Schultheiss <complaint@jonasschultheiss.dev>
2024-10-09 12:07:20 +0200

    fix: export static next site

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 import nextra from 'nextra';
 
-export default nextra({
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // mandatory, otherwise won't export
+  },
+  distDir: 'dist',
+};
+
+const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-})();
+});
+
+export default withNextra(nextConfig);
