1f57fb8601a37244219ce9b3a90504f4b73f446e
Jonas Schultheiss <complaint@jonasschultheiss.dev>
2024-10-10 09:00:09 +0200

    feat: adds basic folder and meta structure

--- apps/docs/pages/en/_meta.ts
+++ apps/docs/pages/en/_meta.ts
@@ -1,15 +1,6 @@
 export default {
-  index: 'Introduction',
-  another: 'Another Page',
-  advanced: 'Advanced (A Folder)',
-  about: {
-    title: 'About',
-    type: 'page',
-  },
-  contact: {
-    title: 'Contact ↗',
-    type: 'page',
-    href: 'https://twitter.com/shuding_',
-    newWindow: true,
-  },
+  index: 'What this document is about',
+  abstract: 'Abstract',
+  branching: 'Branching strategy',
+  versioning: 'Versioning strategy',
 };
---
+++ apps/docs/pages/en/branching/_meta.ts
@@ -0,0 +1,3 @@
+export default {
+  index: 'Why we need one',
+};
